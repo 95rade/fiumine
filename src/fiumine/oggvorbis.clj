a39f7ff8c7f69536b6ed7d213cd54830d8073f4f
Chris Rossi <chris@archimedeanco.com>
2013-07-30 17:13:31 -0400

    Read ogg stream page by page.

--- src/fiumine/oggvorbis.clj
+++ src/fiumine/oggvorbis.clj
@@ -2,30 +2,51 @@
   "Provides some logical chunking of ogg vorbis streams."
   )
 
-(declare scan-for-page!)
+(declare ogg-page!)
 
 (defn oggvorbis-stream
   "???"
   [stream]
-  "???"
-  )
-
-(def ogg-capture (map int "Oggs"))
-(def read-ahead 4096) ; SWAG
+  (when-let [page (ogg-page! stream)]
+    (cons page (lazy-seq (oggvorbis-stream stream)))))
 
 (defn scan-for-page!
-  "Scans ahead in Ogg Vorbis stream looking for capture sequence"
+  "Scans ahead in Ogg Vorbis stream looking for capture sequence. After calling
+   this function, the stream will either be at the beginning of an Ogg page, or
+   at the end of the stream."
   [stream]
+  (.mark stream 4)
   (loop [run 0]
-    (let [ch (.read stream)]
+    (let [ch (.read stream)
+          capture-sequence (map int "OggS")]
       (if (= ch -1) 
-        stream
+        nil
         (if (= run 4)
           (do 
             (.reset stream)
             stream)
-          (if (= ch (nth ogg-capture run))
+          (if (= ch (nth capture-sequence run))
             (recur (inc run))
             (do
-              (.mark stream read-ahead)
+              (.mark stream 4)
               (recur 0))))))))
+
+(def max-ogg-header-size (+ 0xff 28))
+
+(defn ogg-page!
+  "Loads a single page from an Ogg stream into a byte array, returning the byte
+   array, or nil if at end of stream."
+  [stream]
+  (if (scan-for-page! stream)
+    (do
+      (.mark stream max-ogg-header-size)
+      ; Skip first 26 bytes
+      (dotimes [_ 26] (.read stream))
+      (let [page-segments (.read stream)
+            data-size (apply + (repeatedly page-segments #(.read stream)))
+            page-size (+ data-size page-segments 27)
+            buffer (byte-array page-size)]
+        (.reset stream)
+        (.read stream buffer)
+        buffer))
+    nil))
--- test/fiumine/oggvorbis_test.clj
+++ test/fiumine/oggvorbis_test.clj
@@ -1,5 +1,6 @@
 (ns fiumine.oggvorbis-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.java.io :refer :all]
+            [clojure.test :refer :all]
             [fiumine.oggvorbis :refer :all])
   (:import (java.io ByteArrayInputStream)))
 
@@ -12,7 +13,26 @@
 
 (deftest test-scan-for-page
   (testing "Scan for beginning of ogg page."
-    (let [data "foobarOggsbazboo"
+    (let [data "foobarOggSbazboo"
           stream (ByteArrayInputStream. (byte-array (map byte data)))]
       (scan-for-page! stream)
-      (is (= "Oggsbaz" (apply str (take 7 (char-seq stream))))))))
+      (is (= "OggSbaz" (apply str (take 7 (char-seq stream))))))))
+
+(deftest test-ogg-page
+  (testing "Test reading a page leaves stream on a page boundary."
+    (let [url (resource "fiumine/test.ogg")
+          stream (input-stream url)
+          page (ogg-page! stream)
+          header (fn [bs] (apply str (map char (take 4 bs))))]
+      (is (= "OggS" (header page)))
+      (is (= "OggS" (header (char-seq stream)))))))
+
+(deftest test-oggvorbis-stream
+  (testing "Test that we get right number of pages."
+    (let [url (resource "fiumine/test.ogg")
+          stream (input-stream url)
+          ogg (oggvorbis-stream stream)
+          pages (vec ogg)]
+      (is (= 142 (.size pages))))))
+
+

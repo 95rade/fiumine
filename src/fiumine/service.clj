1f3e7e372dfc75fa58cb9ef791402b7dbe61d0fb
Chris Rossi <chris@archimedeanco.com>
2013-08-07 16:01:03 -0400

    Print exceptions to console.

--- src/fiumine/service.clj
+++ src/fiumine/service.clj
@@ -4,17 +4,27 @@
             [ring.util.io :as io]
             [fiumine.publisher :as pub]))
 
+(defn- stream-pages
+  "Returns a function to be called by Ring's piped input stream implementation
+   which writes pages to the response output stream."
+  [pages]
+  (fn [out]
+    (try
+      (fn [out]
+        (loop [pages pages]
+          (when-let [page (first pages)]
+            (.write out (:page-data page))
+            (recur (rest pages)))))
+      (catch Exception e 
+        (prn e) 
+        (throw e)))))
+
 (defn- make-handler [publisher]
-  (defn- handler [request]
+  (fn [request]
     (let [pages @(pub/subscribe publisher)]
       {:status 200
        :headers {"Content-Type" "application/ogg"}
-       :body (io/piped-input-stream 
-               (fn [out]
-                 (loop [pages pages]
-                   (when-let [page (first pages)]
-                     (.write out (:page-data page))
-                     (recur (rest pages))))))})))
-
+       :body (io/piped-input-stream (stream-pages pages))})))
+    
 (defn start [publisher port]
   (jetty/run-jetty (make-handler publisher) {:port port :max-threads 1000}))
